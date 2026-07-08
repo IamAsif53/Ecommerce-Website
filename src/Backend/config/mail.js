05624ba7c72a389e47bf15c4cafd90bc8835c2f8
Jiaul Asif <u2104053@student.cuet.ac.bd>
2026-07-09 00:41:05 +0600

    mail system checked

--- src/Backend/config/mail.js
+++ src/Backend/config/mail.js
@@ -3,12 +3,21 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // STARTTLS
-  family: 4, // Force IPv4
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+console.log("SMTP CONFIG");
+console.log("HOST:", "smtp.gmail.com");
+console.log("PORT:", 587);
+console.log("USER:", process.env.EMAIL_USER);
+
+await transporter
+  .verify()
+  .then(() => console.log("SMTP VERIFIED"))
+  .catch((err) => console.error("SMTP VERIFY ERROR:", err));
 
 export default transporter;
